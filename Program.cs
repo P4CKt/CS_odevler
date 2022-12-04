936f3a6543c826fcc5a21e55c676ce89cbbeb943
P4CKt <116915510+P4CKt@users.noreply.github.com>
2022-12-05 01:20:21 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace odev_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hİii");
+    
+
+        
+        }
+    }
+}
